90485330f44e205fce99ec7b4b0cbf4dd1163e15
ThomasLee03 <125076120+ThomasLee03@users.noreply.github.com>
2024-04-29 23:11:36 -0400

    Add files via upload

--- accuracy bar.py
+++ accuracy bar.py
@@ -7,26 +7,9 @@ barWidth = 0.25
 fig = plt.subplots(figsize =(12, 8)) 
  
 # set height of bar 
-TRAcc = [0.9397007042253521, 1.0, 0.9036091549295775, 1.0] 
-TAcc = [ 0.8867605633802817, 0.7887323943661971, 0.8626760563380281, .8767605633802817] 
+TRAcc = [0.8309859154929577, 0.8912852112676056,0.8908450704225352,  0.9392605633802817 , 0.9194542253521126, 0.9397007042253521] 
+TAcc = [0.7904929577464789, 0.8690845070422535, 0.8655633802816901, 0.8838028169014085, 0.8785211267605634, 0.8867605633802817] 
 
-#categorical NB
-
-#Training accuracy:  0.9036091549295775
-#Test accuracy:  0.8626760563380281
-
-#random forest:
-    
-#Training accuracy:  1.0
-#Test accuracy:  0.8767605633802817    
-
-
-#decision tree:
-    
-#Training accuracy:  1.0
-#Test accuracy:  0.7887323943661971
-
- 
 # Set position of bar on X axis 
 br1 = np.arange(len(TRAcc)) 
 br2 = [x + barWidth for x in br1]  
@@ -42,12 +25,12 @@ plt.bar(br2, TAcc, color ='g', width = barWidth,
 plt.xlabel('Model Used', fontweight ='bold', fontsize = 15) 
 plt.ylabel('Accuracy', fontweight ='bold', fontsize = 15) 
 plt.xticks([r + barWidth-.125 for r in range(len(TRAcc))], 
-        ['LR: nc (OHE, l2)', 'DTC', 'Cat NB', 'Random Forest'])
+        ['LR LBFGS','LR nc', 'LR nc(l2)', 'LR nc(OHE)', 'LR PCA nc(OHE, l2)', 'LR nc(OHE, l2)'])
  
  
 
-plt.ylim([.51, 1.1])
+plt.ylim([.51, .95])
 
-plt.title('Training and Testing Accuracy for different models')
+plt.title('Training and Testing Accuracy for Logistic Regression model Variations')
 plt.legend()
 plt.show() 
